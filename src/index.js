f41e9e4c4a28df5d51a3f186e963525842bffb94
liyh <lyh106415@alibaba-inc.com>
2017-01-08 23:41:16 +0800

    feat: add webpack hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-const EmiliaPlugin = class {
+export default class EmiliaPlugin {
   apply(compiler) {
+    const options = this.options || {}
 
+    compiler.plugin('normal-module-factory', function (nmf) {
+      nmf.plugin('before-resolve', function (data, callback) {
+        // try to get spite hash
+        callback(null, data)
+      })
+    })
+
+    compiler.plugin('compilation', function (compilation) {
+      compilation.plugin('normal-module-loader', function (loaderContext, module) {
+        // loaderContext.spriteHash = 'sprite
+      })
+
+      compilation.plugin('optimize-modules', function (modules) {})
+    })
+
+    compiler.plugin('emit', (compilation, callback) => {
+      callback()
+    })
   }
 }
-
-module.exports = EmiliaPlugin
