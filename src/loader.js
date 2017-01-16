4a5ae56f1f92cd66c3f9883aec27fc3a32c23a4b
cupools <cupools@gmail.com>
2017-01-16 23:53:18 +0800

    feat: terrible loader currently

--- src/loader.js
+++ src/loader.js
@@ -1,9 +1,49 @@
 'use strict'
 
-const loaderUtils = require('loader-utils')
-const assign = require('lodash.assign')
+import path from 'path'
+import postcss from 'postcss'
+import loaderUtils from 'loader-utils'
+import storage from './storage'
 
 module.exports = function (content) {
-  const config = loaderUtils.getLoaderConfig(this, this.options ? 'pxrem' : '')
-  return content
+  const config = loaderUtils.getLoaderConfig(this, this.options ? 'emilia' : '')
+  const root = postcss.parse(content)
+  const context = this.emiliaContext
+
+  root.walkDecls(/background(-image)?$/, decl => {
+    const { value } = decl
+    const start = value.indexOf('url(')
+    const end = value.indexOf(')')
+    const [url, tag] = value.substring(start + 4, end).replace(/^[\s"']|[\s"']$/g, '').split('?__')
+    if (!tag) {
+      return
+    }
+
+    const realpath = path.join(context, url)
+    const result = storage.get(tag)
+    if (!result) {
+      return
+    }
+
+    const coordinate = result.filter(item => item.meta === realpath)[0]
+    if (!coordinate) {
+      return
+    }
+
+    const parent = decl.parent
+    const size = result.width + 'px ' + result.height + 'px'
+    const position = (0 - coordinate.x) + 'px ' + (0 - coordinate.y) + 'px'
+
+    parent.walkDecls(/background-(size|position)/, d => d.remove())
+    parent.insertAfter(decl, {
+      prop: 'background-position',
+      value: position
+    })
+    parent.insertAfter(decl, {
+      prop: 'background-size',
+      value: size
+    })
+  })
+
+  return root.toString()
 }
